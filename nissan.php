c01537a39261175c068d8f469ac9748246e73471
kenta watanabe <kenta.watanabe@tokyo.supersoftware.co.jp>
2019-07-17 19:20:59 +0900

    modified nissan.php

--- nissan.php
+++ nissan.php
@@ -6,13 +6,6 @@ class Nissan extends Car {
         $this->price = 1500000;
         $this->capacity = 5;
         $this->speed = 160;
-    }
-
-    public function defect() {
-        $this->maker = "Nissan";
-        $this->price = 1500000;
-        $this->capacity = 5;
-        $this->speed = 160;
         $this->speed *= 0.6;
     }
 }
